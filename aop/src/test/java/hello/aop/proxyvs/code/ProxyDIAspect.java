15b507fc53a9b065b70284bea37d85c892958e1f
younghokang <younghokang.1121@gmail.com>
2022-10-11 23:58:29 +0900

    cglib vs jdk proxy

---
+++ aop/src/test/java/hello/aop/proxyvs/code/ProxyDIAspect.java
@@ -0,0 +1,18 @@
+package hello.aop.proxyvs.code;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+public class ProxyDIAspect {
+	
+	@Before("execution(* hello.aop..*.*(..))")
+	public void doTrace(JoinPoint joinPoint) {
+		log.info("[proxyDIAdvice] {}", joinPoint.getSignature());
+	}
+
+}
