192724b9b10f045ecee2fb087456b33bf550202a
younghokang <younghokang.1121@gmail.com>
2022-10-11 20:43:08 +0900

    pointcut commit

---
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,55 @@
+package hello.aop;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
+public class AopTest {
+	
+	@Autowired
+	OrderService orderService;
+	
+	@Autowired
+	OrderRepository orderRepository;
+	
+	@Test
+	void aopInfo() {
+		log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+		log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+	}
+	
+	@Test
+	void success() {
+		orderService.orderItem("itemA");
+	}
+	
+	@Test
+	void exception() {
+		assertThrows(IllegalStateException.class, () -> {
+			orderService.orderItem("ex");
+		});
+	}
+
+}
